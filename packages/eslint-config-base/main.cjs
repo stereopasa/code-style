a1ceb9b50edf360d3855895c2c11985c2c566848
stereopasa <stereopasa@gmail.com>
2023-01-14 23:48:22 +0200

    rm redundant extends in esllint config

--- packages/eslint-config-base/main.cjs
+++ packages/eslint-config-base/main.cjs
@@ -2,7 +2,6 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',

3bc46e22cdaf41284aaf876b3593833aeddc571b
stereopasa <stereopasa@gmail.com>
2023-04-03 00:05:10 +0300

    Publish

--- packages/eslint-config-base/main.cjs
+++ packages/eslint-config-base/main.cjs
@@ -1,12 +1,17 @@
+// This is a patch so that eslint will load the plugins as dependencies. Otherwise we can to install EVERYTHING in th root project
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+/** @type{import("eslint").Linter.Config} */
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'prettier',
   ],
   parser: require.resolve('@typescript-eslint/parser'),
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'import'],
   reportUnusedDisableDirectives: true,
   rules: {},
   overrides: [

30a668959442e968b05398620d06661ddf7b36ac
stereopasa <stereopasa@gmail.com>
2023-07-08 11:36:01 +0300

    chore: update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,5 @@
-module.exports = require('./packages/eslint-config-base/main.cjs');
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: './packages/eslint-config-base/main.cjs',
+  parserOptions: { tsconfigRootDir: __dirname },
+};

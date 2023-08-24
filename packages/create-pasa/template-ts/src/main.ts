e345c49c4c8eaed3ffc9d611ef9c47da4789ebbe
stereopasa <stereopasa@gmail.com>
2023-08-24 12:33:06 +0300

    chore(create): update deps

--- packages/create-pasa/template-ts/src/main.ts
+++ packages/create-pasa/template-ts/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 import typescriptLogo from './typescript.svg';
+// _eslint-disable-next-line import/no-unresolved
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
 

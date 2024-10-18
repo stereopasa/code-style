fc90800e4b1f720541e0cffefe1775dad8b35bc1
stereopasa <stereopasa@gmail.com>
2024-10-18 12:39:56 +0300

    feat(create): update ts template

---
+++ .changeset/pink-shoes-own.md
@@ -0,0 +1,5 @@
+---
+'create-pasa': patch
+---
+
+feat(template): update ts template
--- packages/create-pasa/template-ts/src/main.ts
+++ packages/create-pasa/template-ts/src/main.ts
@@ -1,8 +1,8 @@
 import './style.css';
-import typescriptLogo from './typescript.svg';
-// _eslint-disable-next-line import/no-unresolved
-import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
+import typescriptLogo from './typescript.svg';
+
+import viteLogo from '/vite.svg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
